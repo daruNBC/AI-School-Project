7e8773be03847908461339aa2fb6e1266e1b68bc
daru <73023071+daruNBC@users.noreply.github.com>
2022-08-07 20:13:16 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import os
+from deeplearning import OCR
+
+# webserver gateway interface
+
+app = Flask(__name__, template_folder="./templates", static_folder="./static")
+
+BASE_PATH = os.getcwd()
+UPLOAD_PATH = os.path.join(
+    BASE_PATH, 'static/upload')
+
+
+@app.route('/', methods=['POST', 'GET'])
+def index():
+    if request.method == 'POST':
+        upload_file = request.files['image_name']
+        filename = upload_file.filename
+        path_save = os.path.join(UPLOAD_PATH, filename)
+        upload_file.save(path_save)
+        text = OCR(path_save, filename)
+
+        return render_template('index.html', upload=True, upload_image=filename, text=text)
+
+    return render_template('index.html', upload=False)
+
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
+
+if __name__ == "__main__":
+    app.run()
